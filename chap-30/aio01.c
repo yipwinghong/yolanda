8cd710ec9e4b94c2467ec66f810c07f118e06c34
yipwinghong <yipwinghong@outlook.com>
2020-08-12 16:52:30 +0800

    :pencil: Add comments

--- chap-30/aio01.c
+++ chap-30/aio01.c
@@ -3,6 +3,7 @@
 
 const int BUF_SIZE = 512;
 
+// Linux 下的 aio 操作不是由系统支持的，只是由 GNU libc 库函数在用户空间借由 pthread 方式实现的，而且仅仅针对磁盘类 I/O，套接字 I/O 不支持。
 int main() {
     int err;
     int result_size;
@@ -21,26 +22,26 @@ int main() {
     char buf[BUF_SIZE];
     struct aiocb aiocb;
 
-    //初始化buf缓冲，写入的数据应该为0xfafa这样的,
+    // 初始化 buf 缓冲，写入的数据应该为 0xfafa 这样的,
     memset(buf, 0xfa, BUF_SIZE);
     memset(&aiocb, 0, sizeof(struct aiocb));
     aiocb.aio_fildes = fd;
     aiocb.aio_buf = buf;
     aiocb.aio_nbytes = BUF_SIZE;
 
-    //开始写
+    // 开始写
     if (aio_write(&aiocb) == -1) {
         printf(" Error at aio_write(): %s\n", strerror(errno));
         close(fd);
         exit(1);
     }
 
-    //因为是异步的，需要判断什么时候写完
+    // 因为是异步的，需要判断什么时候写完
     while (aio_error(&aiocb) == EINPROGRESS) {
         printf("writing... \n");
     }
 
-    //判断写入的是否正确
+    // 判断写入的是否正确
     err = aio_error(&aiocb);
     result_size = aio_return(&aiocb);
     if (err != 0 || result_size != BUF_SIZE) {
@@ -49,7 +50,7 @@ int main() {
         exit(1);
     }
 
-    //下面准备开始读数据
+    // 下面准备开始读数据
     char buffer[BUF_SIZE];
     struct aiocb cb;
     cb.aio_nbytes = BUF_SIZE;
@@ -63,7 +64,7 @@ int main() {
         close(fd);
     }
 
-    //因为是异步的，需要判断什么时候读完
+    // 因为是异步的，需要判断什么时候读完
     while (aio_error(&cb) == EINPROGRESS) {
         printf("Reading... \n");
     }
